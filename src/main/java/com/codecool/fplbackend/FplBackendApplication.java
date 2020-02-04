03f61fedcf990c138d4cc6c11b619df7ce14b028
David Korpos <korposdavid@gmail.com>
2020-02-04 15:01:29 +0100

    Initial commit

---
+++ src/main/java/com/codecool/fplbackend/FplBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.fplbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FplBackendApplication.class, args);
+    }
+
+}
