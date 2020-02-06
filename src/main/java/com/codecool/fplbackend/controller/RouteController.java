dc602e3b5fb9d05cd84bb1b6b6e4a28d19075e0d
David Korpos <korposdavid@gmail.com>
2020-02-06 11:36:59 +0100

    create route for getting user's specific team

--- src/main/java/com/codecool/fplbackend/controller/RouteController.java
+++ src/main/java/com/codecool/fplbackend/controller/RouteController.java
@@ -1,6 +1,8 @@
 package com.codecool.fplbackend.controller;
 
 import com.codecool.fplbackend.model.FPLUserData;
+import com.codecool.fplbackend.model.Player;
+import com.codecool.fplbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -9,12 +11,17 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Set;
+
 @RestController
 @CrossOrigin
 public class RouteController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/fpl-api-login")
     public HttpHeaders ApiLogin(@RequestBody FPLUserData fplUserData){
@@ -33,5 +40,10 @@ public class RouteController {
                 String.class
         );
         return response.getHeaders();
+    }
+
+    @GetMapping("/my-team/{id}")
+    public Set<Player> getTeam(@PathVariable("id") Long id){
+        return userRepository.getOne(id).getTeam();
     }
 }
