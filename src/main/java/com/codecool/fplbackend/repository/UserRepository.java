37ebc9c57a0c292c6e8a2aa14d495451482f365d
David Korpos <korposdavid@gmail.com>
2020-02-06 11:25:26 +0100

    init UserRepository

---
+++ src/main/java/com/codecool/fplbackend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.codecool.fplbackend.repository;
+
+import com.codecool.fplbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
