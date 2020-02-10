2bde5aabb668ffc1df90ad9899bdf2a0ac7f1b67
David Korpos <korposdavid@gmail.com>
2020-02-10 11:05:10 +0100

    remove unused api login method

--- src/main/java/com/codecool/fplbackend/service/FPLApiService.java
+++ src/main/java/com/codecool/fplbackend/service/FPLApiService.java
@@ -3,12 +3,8 @@ package com.codecool.fplbackend.service;
 import com.codecool.fplbackend.model.FPLData;
 import com.codecool.fplbackend.model.Player;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -19,24 +15,6 @@ public class FPLApiService {
 
     @Autowired
     private RestTemplate restTemplate;
-
-    public HttpHeaders login(String email, String password) {
-        MultiValueMap<String, String> requestData = new LinkedMultiValueMap<>();
-        requestData.add("login", email);
-        requestData.add("password", password);
-        requestData.add("redirect_uri", "https://fantasy.premierleague.com/a/login");
-        requestData.add("app", "plfpl-web");
-
-        HttpHeaders requestHeaders = new HttpHeaders();
-        HttpEntity<MultiValueMap<String, String>> httpRequest = new HttpEntity<>(requestData, requestHeaders);
-
-        HttpEntity<String> response = restTemplate.postForEntity(
-                "https://users.premierleague.com/accounts/login/",
-                httpRequest,
-                String.class
-        );
-        return response.getHeaders();
-    }
 
     public List<Player> fetchPlayerData(){
         String url = "https://fantasy.premierleague.com/api/bootstrap-static/";
