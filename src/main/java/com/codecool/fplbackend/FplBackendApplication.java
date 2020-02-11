38586ce690320760beebf8339414591a7bb7d5fb
David Korpos <korposdavid@gmail.com>
2020-02-11 13:30:13 +0100

    delete unused user specific files

--- src/main/java/com/codecool/fplbackend/FplBackendApplication.java
+++ src/main/java/com/codecool/fplbackend/FplBackendApplication.java
@@ -1,9 +1,6 @@
 package com.codecool.fplbackend;
 
-import com.codecool.fplbackend.model.Player;
-import com.codecool.fplbackend.model.User;
 import com.codecool.fplbackend.repository.PlayerRepository;
-import com.codecool.fplbackend.repository.UserRepository;
 import com.codecool.fplbackend.service.FPLApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,15 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 @SpringBootApplication
 public class FplBackendApplication {
-
-    @Autowired
-    private UserRepository userRepository;
-
     @Autowired
     private PlayerRepository playerRepository;
 
@@ -40,14 +30,6 @@ public class FplBackendApplication {
     public CommandLineRunner init() {
         return args -> {
             playerRepository.saveAll(fplApiService.fetchPlayerData());
-
-            List<Long> playerIds = new ArrayList<>(Arrays.asList(47L,181L,65L,128L,297L,191L,215L,342L,150L,67L,187L,93L,164L,468L,271L));
-
-            List<Player> players = playerRepository.findAllById(playerIds);
-
-            User user = User.builder().team(new HashSet<>(players)).username("test_user").build();
-
-            userRepository.save(user);
         };
     }
 }
--- src/main/java/com/codecool/fplbackend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.fplbackend.repository;
-
-import com.codecool.fplbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
