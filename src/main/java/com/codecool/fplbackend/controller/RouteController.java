8fbd47f12e2b539dccdee39350bb0353cd743bde
David Korpos <korposdavid@gmail.com>
2020-02-11 13:27:52 +0100

    remove unused routes, add getPlayer route

--- src/main/java/com/codecool/fplbackend/controller/RouteController.java
+++ src/main/java/com/codecool/fplbackend/controller/RouteController.java
@@ -1,31 +1,19 @@
 package com.codecool.fplbackend.controller;
 
-import com.codecool.fplbackend.model.FPLUserData;
 import com.codecool.fplbackend.model.Player;
-import com.codecool.fplbackend.repository.UserRepository;
-import com.codecool.fplbackend.service.FPLApiService;
+import com.codecool.fplbackend.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Set;
 
 @RestController
 @CrossOrigin
 public class RouteController {
     @Autowired
-    private UserRepository userRepository;
+    private PlayerRepository playerRepository;
 
-    @Autowired
-    private FPLApiService fplApiService;
-
-    @PostMapping("/fpl-api-login")
-    public HttpHeaders ApiLogin(@RequestBody FPLUserData fplUserData){
-        return fplApiService.login(fplUserData.getEmail(),fplUserData.getPassword());
-    }
-
-    @GetMapping("/my-team/{id}")
-    public Set<Player> getTeam(@PathVariable("id") Long id){
-        return userRepository.getOne(id).getTeam();
+    @GetMapping("/player/{id}")
+    public Player getPlayer(@PathVariable("id") Long id){
+        return playerRepository.getOne(id);
     }
 }
