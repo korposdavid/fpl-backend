d04df82ccd9ef387410a31e83c297d7c15d009df
David Korpos <korposdavid@gmail.com>
2020-02-11 14:46:52 +0100

    create fetchFixtureData method

--- src/main/java/com/codecool/fplbackend/service/FPLApiService.java
+++ src/main/java/com/codecool/fplbackend/service/FPLApiService.java
@@ -1,6 +1,7 @@
 package com.codecool.fplbackend.service;
 
 import com.codecool.fplbackend.model.FPLData;
+import com.codecool.fplbackend.model.Fixture;
 import com.codecool.fplbackend.model.Player;
 import com.codecool.fplbackend.model.Team;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -31,5 +33,13 @@ public class FPLApiService {
         ResponseEntity<FPLData> responseData = restTemplate.getForEntity(url, FPLData.class);
 
         return Objects.requireNonNull(responseData.getBody()).getTeams();
+    }
+
+    public List<Fixture> fetchFixtureData(){
+        String url = "https://fantasy.premierleague.com/api/fixtures/";
+
+        ResponseEntity<Fixture[]> responseData = restTemplate.getForEntity(url, Fixture[].class);
+
+        return Arrays.asList(Objects.requireNonNull(responseData.getBody()));
     }
 }
