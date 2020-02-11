0590e101f1495129eca18ecbc81fef98f97bce02
David Korpos <korposdavid@gmail.com>
2020-02-11 14:14:48 +0100

    add getTeam to RouteController

--- src/main/java/com/codecool/fplbackend/controller/RouteController.java
+++ src/main/java/com/codecool/fplbackend/controller/RouteController.java
@@ -1,7 +1,9 @@
 package com.codecool.fplbackend.controller;
 
 import com.codecool.fplbackend.model.Player;
+import com.codecool.fplbackend.model.Team;
 import com.codecool.fplbackend.repository.PlayerRepository;
+import com.codecool.fplbackend.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,8 +14,16 @@ public class RouteController {
     @Autowired
     private PlayerRepository playerRepository;
 
+    @Autowired
+    private TeamRepository teamRepository;
+
     @GetMapping("/player/{id}")
     public Player getPlayer(@PathVariable("id") Long id){
         return playerRepository.getOne(id);
+    }
+
+    @GetMapping("/team/{id}")
+    public Team getTeam(@PathVariable("id") Long id){
+        return teamRepository.getOne(id);
     }
 }
