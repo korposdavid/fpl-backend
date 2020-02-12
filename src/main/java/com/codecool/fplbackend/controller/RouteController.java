56eb2359c9faad17a115a24dddc83e26a7e377ba
David Korpos <korposdavid@gmail.com>
2020-02-12 11:05:00 +0100

    refactor RouteController with DataManager

--- src/main/java/com/codecool/fplbackend/controller/RouteController.java
+++ src/main/java/com/codecool/fplbackend/controller/RouteController.java
@@ -2,8 +2,7 @@ package com.codecool.fplbackend.controller;
 
 import com.codecool.fplbackend.model.Player;
 import com.codecool.fplbackend.model.Team;
-import com.codecool.fplbackend.repository.PlayerRepository;
-import com.codecool.fplbackend.repository.TeamRepository;
+import com.codecool.fplbackend.service.DataManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,23 +13,20 @@ import java.util.List;
 @CrossOrigin
 public class RouteController {
     @Autowired
-    private PlayerRepository playerRepository;
-
-    @Autowired
-    private TeamRepository teamRepository;
+    private DataManager dataManager;
 
     @GetMapping("/player/{id}")
     public Player getPlayer(@PathVariable("id") Long id){
-        return playerRepository.getOne(id);
+        return dataManager.getPlayer(id);
     }
 
     @GetMapping("players/{ids}")
     public List<Player> getPlayers(@PathVariable("ids") List<Long> ids) {
-        return playerRepository.findAllById(ids);
+        return dataManager.getPlayers(ids);
     }
 
     @GetMapping("/team/{id}")
     public Team getTeam(@PathVariable("id") Long id){
-        return teamRepository.getOne(id);
+        return dataManager.getTeam(id);
     }
 }
