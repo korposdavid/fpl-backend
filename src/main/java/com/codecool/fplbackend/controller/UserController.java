d5a529827aedaaee22237c0d91aa0c60605427d7
David Korpos <korposdavid@gmail.com>
2020-02-18 16:41:21 +0100

    refactor logout, add Todo for user lookup in db

--- src/main/java/com/codecool/fplbackend/controller/UserController.java
+++ src/main/java/com/codecool/fplbackend/controller/UserController.java
@@ -5,17 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000"}, allowedHeaders = {"*"},  allowCredentials = "true", methods = {RequestMethod.POST, RequestMethod.OPTIONS, RequestMethod.GET})
 public class UserController {
     private ClientRegistration registration;
 
@@ -28,21 +24,14 @@ public class UserController {
         if (user == null) {
             return new ResponseEntity<>("", HttpStatus.OK);
         } else {
+            //TODO: lookup user by attributes in userrepo => create if not exists
             return ResponseEntity.ok().body(user.getAttributes());
         }
     }
 
     @PostMapping("/api/logout")
-    public ResponseEntity<?> logout(HttpServletRequest request,
-                                    @AuthenticationPrincipal(expression = "idToken") OidcIdToken idToken) {
-        // send logout URL to client so they can initiate logout
-        String logoutUrl = this.registration.getProviderDetails()
-                .getConfigurationMetadata().get("end_session_endpoint").toString();
-
-        Map<String, String> logoutDetails = new HashMap<>();
-        logoutDetails.put("logoutUrl", logoutUrl);
-        logoutDetails.put("idToken", idToken.getTokenValue());
+    public ResponseEntity<?> logout(HttpServletRequest request, @AuthenticationPrincipal OAuth2User user) {
         request.getSession(false).invalidate();
-        return ResponseEntity.ok().body(logoutDetails);
+        return new ResponseEntity<>("", HttpStatus.OK);
     }
 }
